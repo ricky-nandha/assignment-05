9cd2690990cce57055f20e5ac6c402862f65a112
Ricky Nandha <ricky.nandha2@gmail.com>
2025-04-29 12:53:13 +0100

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # assignment-05
+
+The Design Principles I have implemented:
+Balance: Create negative spacing so the users focus is on the text in the center of the screen.
+Type: There is a big clear purpose of the site at the top of the page.
+User flow: There is a clear flow from the top to the bottom of the page.
+Hierachy: The page directs the user towards the form to fill in.
+Consistency: There is the same font and colours used throughout the site.
+Simpicity: The information is clear and chunked together.
+Imagery: big and sparing text with bright colors to associate with a kids vibe.
+Typography: Used comic sans to stay with the childrens vibe.
+
+I have used media queries to make the page adjust to 3 different screen sizes eg mobile, ipad and computer.
+I ensured that content is presented well and the app has some thought given to design, so that it is more appealing to use.
+
+I have ensured that form inputs have got associated descriptions to prevent confusion.
+
+I have met accessibility needs within my page with ARIA labels describing everything and a tab index so the user can move through the page in the correct order and using the keyboard.
+
+I have a form that successfully retrieves user input and then uses fetch requests to get specific data from several APIs by using extra query parameters which gives my app enhanced functionality.
+
+Brand Identity:
+Pastel colours for background and bright colours for information but overal colourful.
+Typography: comics sans for children throguhout page
+Tone: clear and easily understandable.
+Mission: Make fun experiement for children
+Vision: Make a variety of children"s games for online use
+Values: Fun, Innovation, Simplicity
+
+Target Customer Persona:
+Persona name: Tom
+Gender: Male
+Age: 7
+Location: London, UK
+Education Level: Primary school
+Point of Need Challenge: Wants to do a fun activity on their ipad with friends
+Preferred Channel(s): Apps
+Tone of voice: Fun and simple
--- app.js
+++ app.js
@@ -1,1 +1,92 @@
-console.log("hello");
+console.log("Let's predict some details!");
+
+let userName = "";
+
+const apiResultsSection = document.getElementById("apiResults");
+
+async function getPredictions() {
+  apiResultsSection.innerHTML = "";
+
+  const predictionDiv = document.createElement("div");
+  predictionDiv.classList = "result-div";
+
+  const nameTitle = document.createElement("h2");
+  nameTitle.textContent = `Predictions for "${userName}"`;
+  nameTitle.classList = "result-title";
+  predictionDiv.appendChild(nameTitle);
+
+  const ageResult = await fetch(`https://api.agify.io/?name=${userName}`);
+  const ageData = await ageResult.json();
+  console.log(ageData);
+
+  if (ageData.age !== null) {
+    const agePredict = document.createElement("p");
+    agePredict.textContent = `Predicted Age: ${ageData.age}`;
+    predictionDiv.appendChild(agePredict);
+  } else {
+    const uniquePredict = document.createElement("p");
+    uniquePredict.textContent = `This name is so unique, we don't have enough age data for it!`;
+    predictionDiv.appendChild(uniquePredict);
+  }
+
+  const genderResult = await fetch(
+    `https://api.genderize.io/?name=${userName}`
+  );
+  const genderData = await genderResult.json();
+  console.log(genderData);
+
+  if (genderData.gender !== null) {
+    const genderPredict = document.createElement("p");
+    genderPredict.textContent = `Predicted Gender: ${genderData.gender}`;
+    predictionDiv.appendChild(genderPredict);
+  } else {
+    const uniquePredict = document.createElement("p");
+    uniquePredict.textContent = `This name is so unique, we don't have enough gender data for it!`;
+    predictionDiv.appendChild(uniquePredict);
+  }
+
+  const nationalityResult = await fetch(
+    `https://api.nationalize.io/?name=${userName}`
+  );
+  const nationalityData = await nationalityResult.json();
+  console.log(nationalityData);
+
+  if (nationalityData.country.length > 0) {
+    const topCountryId = nationalityData.country[0].country_id;
+
+    const countryResult = await fetch(
+      `https://restcountries.com/v3.1/alpha/${topCountryId}`
+    );
+    const countryData = await countryResult.json();
+    console.log(countryData);
+
+    const countryName = countryData[0].name.common;
+
+    const countryPredict = document.createElement("p");
+    countryPredict.textContent = `Predicted Country: ${countryName}`;
+    predictionDiv.appendChild(countryPredict);
+  } else {
+    const uniquePredict = document.createElement("p");
+    uniquePredict.textContent = `This name is so unique, we don't have enough country data for it!`;
+    predictionDiv.appendChild(uniquePredict);
+  }
+
+  apiResultsSection.appendChild(predictionDiv);
+}
+
+const myApiForm = document.getElementById("apiForm");
+
+myApiForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Form submitted!");
+
+  const formData = new FormData(myApiForm);
+  const formDataObject = Object.fromEntries(formData);
+  console.log(formDataObject);
+
+  userName = formDataObject.userNameInput.trim();
+
+  if (userName) {
+    getPredictions();
+  }
+});
